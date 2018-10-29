a1b55b5341870f8cce1789f3bb42111c8da59c27
BengBu-YueZhang <zhangyue_coder@163.com>
2018-10-29 15:05:45 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
 const loaderUtils = require('loader-utils')
+const defaultOptions = {
+  from: '罗永浩',
+  to: '罗永浩太君'
+}
 
 module.exports = function (source) {
-  const options = loaderUtils.getOptions(this)
-  console.log('***************************')
-  console.log(options)
+  this.cacheable && this.cacheable()
+  const options = Object.assign({}, defaultOptions, loaderUtils.getOptions(this))
+  const { to, from } = options
+  const reg = new RegExp(from)
+  const regResult = reg.exec(source)
+  if (regResult) {
+    source = source.replace(reg, to)
+  }
   return source
 }
